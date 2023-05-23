3d240bfb8a58b5366189c6646ecc48b78a3bb0f0
TlpAyn <talipayan6035@gmail.com>
2023-05-24 00:48:55 +0200

    Initial commit

---
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -0,0 +1,47 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class MyStepdefs {
+    @Given("Navigate to IBM HomePages")
+    public void navigateToIBMHomePages() {
+        
+    }
+
+    @And("Click members button")
+    public void clickMembersButton() {
+        
+    }
+
+    @And("Click MyIBM Button")
+    public void clickMyIBMButton() {
+        
+    }
+
+    @And("put IBMid and click Box")
+    public void putIBMidAndClickBox() {
+        
+    }
+
+
+    @And("click continue btn")
+    public void clickContinueBtn() {
+        
+    }
+
+    @And("Put Password")
+    public void putPassword() {
+        
+    }
+
+    @And("click Sign in")
+    public void clickSignIn() {
+        
+    }
+
+    @Then("User should login successfully")
+    public void userShouldLoginSuccessfully() {
+    }
+}
